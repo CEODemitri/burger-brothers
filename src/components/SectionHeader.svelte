4da06cd06f72d54b2e92e08c41ac2642e328e059
ceoDemitri <demichole@gmail.com>
2024-05-08 21:40:07 -0500

    sm margin to section header component

--- src/components/SectionHeader.svelte
+++ src/components/SectionHeader.svelte
@@ -1,22 +1,22 @@
 <script>
-    export let pill;
-    export let title;
-    export let description;
+	export let pill;
+	export let title;
+	export let description;
 </script>
 
-<div class="text-center text-dark flex flex-col mb-6">
-    <p class="pill p-5">{pill}</p>
-    <h1 class="text-dark ff-sans-normal letter-spacing-3 fs-600">{title}</h1>
-    <p class="fs-200 ff-sans w-[55ch] m-auto">{description}</p>
+<div class="text-center text-dark flex flex-col mb-6 mt-10">
+	<p class="pill p-5">{pill}</p>
+	<h1 class="text-dark ff-sans-normal letter-spacing-3 fs-600">{title}</h1>
+	<p class="fs-200 ff-sans w-[55ch] m-auto">{description}</p>
 </div>
 
 <style>
-    p.pill {
-        background-color: hsl(250, 27%, 17%);
+	p.pill {
+		background-color: hsl(250, 27%, 17%);
 		color: #fff;
 		width: fit-content;
 		padding: 5px;
 		border-radius: 15px;
 		margin: auto;
-    }
+	}
 </style>
