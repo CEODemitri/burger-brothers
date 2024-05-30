aad6a381a13647da77129c2f4e905da86444d572
ceoDemitri <demichole@gmail.com>
2024-05-30 18:15:55 -0500

    slowly building more of the design page

--- src/routes/Design/+page.svelte
+++ src/routes/Design/+page.svelte
@@ -1,13 +1,48 @@
-<div class="text-dark">
+<div class="text-dark p-5">
 	<h1>Design System</h1>
 
 	<section>
 		<h2><span class="text-sm">I</span>COLORS</h2>
+
+		<div>
+			<!-- 1 color -->
+			<article>
+				<!-- color -->
+				<section></section>
+
+				<!-- name and description -->
+				<p></p>
+				<p></p>
+			</article>
+
+			<!-- 1 color -->
+			<article>
+				<!-- color -->
+				<section></section>
+
+				<!-- name and description -->
+				<p></p>
+				<p></p>
+			</article>
+
+			<!-- 1 color -->
+			<article>
+				<!-- color -->
+				<section></section>
+
+				<!-- name and description -->
+				<p></p>
+				<p></p>
+			</article>
+		</div>
 	</section>
 </div>
 
 <style>
-    h2 span {
-        margin-right: .5em;
-    }
+	h2 span {
+		margin-right: 0.5em;
+		background-color: orange;
+		padding: 0.6em;
+		border-radius: 7px 0 7px 0;
+	}
 </style>
