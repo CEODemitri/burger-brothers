9261338e67c84dfcfcbd99f0f4c0ee574f57ffe1
ceoDemitri <demichole@gmail.com>
2024-07-28 13:23:01 -0500

    fix navbar with daisy ui component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,93 +4,101 @@
 	import '@fontsource/finger-paint';
 </script>
 
-<header class="flex justify-between items-center px-5 m-auto bg-[#000]">
-	<nav class="flex gap-4 sm:gap-6 text-dark uppercase items-center">
-		<svg width="36" height="16" viewBox="0 0 46 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M0 3H46" stroke="#BC5C28" stroke-width="4" />
-			<path d="M0 18H46" stroke="#BC5C28" stroke-width="4" />
-			<path d="M0 33H46" stroke="#BC5C28" stroke-width="4" />
-		</svg>
+<div class="navbar bg-[#000]">
+	<div class="navbar-start">
+		<nav class="flex gap-4 sm:gap-6 text-dark uppercase items-center">
+			<svg
+				width="36"
+				height="16"
+				viewBox="0 0 46 36"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path d="M0 3H46" stroke="#BC5C28" stroke-width="4" />
+				<path d="M0 18H46" stroke="#BC5C28" stroke-width="4" />
+				<path d="M0 33H46" stroke="#BC5C28" stroke-width="4" />
+			</svg>
 
-		<a
-			class="hover:underline underline-offset-4 hover:scale-105 hover:font-extrabold hidden md:block"
-			href="/Menu"
-			rel="ugc"
-		>
-			Menu
+			<a
+				class="hover:underline underline-offset-4 hover:scale-105 hover:font-extrabold hidden md:block"
+				href="/Menu"
+				rel="ugc"
+			>
+				Menu
+			</a>
+			<a
+				class="hover:underline underline-offset-4 hover:scale-105 hover:font-extrabold hidden md:block"
+				href="/Services"
+				rel="ugc"
+			>
+				Services
+			</a>
+			<a
+				class="hover:underline underline-offset-4 hover:scale-105 hover:font-extrabold hidden md:block"
+				href="/About"
+				rel="ugc"
+			>
+				About
+			</a>
+			<a
+				class="hover:underline underline-offset-4 hover:scale-105 hover:font-extrabold hidden md:block"
+				href="/Events"
+				rel="ugc"
+			>
+				FAQ
+			</a>
+		</nav>
+	</div>
+	<div class="navbar-center">
+		<a class="flex items-center justify-center" href="/" rel="ugc">
+			<figure>
+				<img src="../logo.png" alt="" class="w-14" />
+				<figcaption>
+					<h1 class="text-2xl font-bold text-gray-900 sr-only">Burger Brothers Logo</h1>
+				</figcaption>
+			</figure>
+			<span class="sr-only">Burger Brothers</span>
 		</a>
-		<a
-			class="hover:underline underline-offset-4 hover:scale-105 hover:font-extrabold hidden md:block"
-			href="/Services"
-			rel="ugc"
-		>
-			Services
-		</a>
-		<a
-			class="hover:underline underline-offset-4 hover:scale-105 hover:font-extrabold hidden md:block"
-			href="/About"
-			rel="ugc"
-		>
-			About
-		</a>
-		<a
-			class="hover:underline underline-offset-4 hover:scale-105 hover:font-extrabold hidden md:block"
-			href="/Events"
-			rel="ugc"
-		>
-			FAQ
-		</a>
-	</nav>
-
-	<a class="flex items-center justify-center" href="/" rel="ugc">
-		<figure>
-			<img src="../logo.png" alt="" class="w-14" />
-			<figcaption>
-				<h1 class="text-2xl font-bold text-gray-900 sr-only">Burger Brothers Logo</h1>
-			</figcaption>
-		</figure>
-		<span class="sr-only">Burger Brothers</span>
-	</a>
-
-	<section class="flex">
+	</div>
+	<div class="navbar-end">
+		<button class="btn btn-ghost btn-circle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+				/>
+			</svg>
+		</button>
+		<button class="btn btn-ghost btn-circle">
+			<div class="indicator">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+					/>
+				</svg>
+				<span class="badge badge-xs badge-primary indicator-item"></span>
+			</div>
+		</button>
 		<button class="bg-action text-xs md:text-sm px-5 py-2 ff-serif">Order Online</button>
-		<!-- <label class="cursor-pointer grid place-items-center">
-			<input
-				type="checkbox"
-				value="synthwave"
-				class="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
-			/>
-			<svg
-				class="col-start-1 row-start-1 stroke-base-100 fill-base-100"
-				xmlns="http://www.w3.org/2000/svg"
-				width="14"
-				height="14"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				><circle cx="12" cy="12" r="5" /><path
-					d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
-				/></svg
-			>
-			<svg
-				class="col-start-2 row-start-1 stroke-base-100 fill-base-100"
-				xmlns="http://www.w3.org/2000/svg"
-				width="14"
-				height="14"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg
-			>
-		</label> -->
-	</section>
-</header>
+	</div>
+</div>
 <slot />
 
 <footer
